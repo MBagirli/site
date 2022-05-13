499cc15bb05134c29fe6d7a4847ab437a33a6b66
MBagirli <104429881+MBagirli@users.noreply.github.com>
2022-05-13 23:58:25 +0400

    Delete press.js

--- press.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#Menu');
-let menu2 = document.querySelector('#Menu2');
-let Nav = document.querySelector('.Navigation');
-menu.addEventListener('click', function () {
-  menu2.classList.toggle('Open');
-  Nav.classList.toggle('Navigation__display');
-});

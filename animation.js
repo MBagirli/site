38a67ab22822e6cdafcc8a2ddea245742ad4c840
MBagirli <104429881+MBagirli@users.noreply.github.com>
2022-05-14 00:02:02 +0400

    Delete animation.js

--- animation.js
+++
@@ -1,7 +0,0 @@
-let allSections = document.querySelectorAll('section');
-let arrayofSections = [];
-allSections.forEach(function (element) {
-  window.addEventListener('scroll', function () {
-    element.classList.add('animationforall');
-  });
-});
